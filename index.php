085c7bb3484083b19f16072e9d50e0e849ca8ba3
Maerill <efriem@WS-INF-STUD-04.bbcnet.ch>
2015-05-11 11:25:39 +0200

    Added html default structure

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-hallo
+<!--
+Startpage, every other page gets loaded in.
+-->
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <?php require_once("resources/head.php"); ?>
+  </head>
+  <body>
+  <div class="header">
+  	<?php require_once("resources/pageHeader.php"); ?>
+  </div>
+  <div class="content">
+	<h1>Willkommen bei Scip</h1>
+	<?php
+		// Wo sind die Seiten?
+		$seitenordner = 'sites/';
+		$defaultseite = 'home';
+
+		if (!empty($_GET['site'])) {	// Wurde eine Seite uebergeben
+			$seite = $_GET["site"];
+		} else {						// Standardseite
+			$seite = $defaultseite;
+		}
+
+		// Gibt es die Datei wirklich
+		if (!file_exists($seitenordner . $seite .".php")) {
+            $seite = $defaultseite.".php";
+        }
+
+        // Inhalt einlesen
+        include $seitenordner . basename($seite.".php");
+        ?>
+  </div>
+  	<?php require_once("resources/footer.php"); ?>
+  </body>
+</html>
---
+++ resources/head.php
@@ -0,0 +1,3 @@
+<meta charset="utf-8">
+<title>Scip</title>
+<link rel="stylesheet" href="resources/defaultstyle.css" type="text/css">
