cf8e75a52185fe06c0f4938dd77ee2c83e1bb5a5
Maerill <efriem@WS-INF-STUD-04.bbcnet.ch>
2015-05-11 14:40:44 +0200

    Added bootstrap

--- resources/head.php
+++ resources/head.php
@@ -1,3 +1,4 @@
 <meta charset="utf-8">
 <title>Scip</title>
 <link rel="stylesheet" href="resources/defaultstyle.css" type="text/css">
+<link rel="stylesheet" type="text/css" href="bootstrap-3.3.4-dist/css/bootstrap.css">
--- sites/home.php
+++ sites/home.php
@@ -1,1 +1,2 @@
 <h1>Willkommen bei Scip</h1>
+<button class="btn btn-default" type="submit">Press Me!</button>
