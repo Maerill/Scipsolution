6b60aaaf12f6f9fbaa83de52ea72390d79dbf072
Raphiii <raphael.spanyar@bluewin.ch>
2015-05-11 16:53:19 +0200

    updated footer

--- resources/footer.php
+++ resources/footer.php
@@ -1,3 +1,6 @@
 <footer>
-  
+	<p style="font-family: Arial,Helvetica,sans-serif; color: #ffffff; font-size: 12px;">
+		<span><img src="pictures/logo.png" width="104" height="90" alt="logo" /></span>
+		<span style="font-family: helvetica; font-size: 18px;">Scip GmbH | Bahnhöheweg 70 | 3018 Bern | © MFR & RSP</span>
+	</p>
 </footer>
