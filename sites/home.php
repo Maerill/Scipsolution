094e1b9db346fccf0bb86b01aa89a3527645fe2f
Maerill <efriem@WS-INF-STUD-04.bbcnet.ch>
2015-05-12 16:55:50 +0200

    edit loginpage

--- sites/home.php
+++ sites/home.php
@@ -1,25 +1,23 @@
 <!-- Login Formular -->
-<div class="center_picture">
+<div class="home-picture">
   <img class="" src="pics/defaultProfilePic.png" width="200">
 </div>
-<div class="center_2">
-  <form action="index.php" method="post" name="login_form" class="form-horizontal">
-    <div class="control-group">
-      <label for="inputEmail3" class="col-sm-5 control-label">Username:</label>
-      <div class="col-sm-2">
-        <input type="username" id="inputUsername" name="login_username" placeholder="Username" required>
+<div class="home-form">
+  <div class="home-heigth">
+    <div class="form-group">
+      <label for="username" class="col-sm-2 control-label">Username:</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" id="username" placeholder="Enter your username">
       </div>
     </div>
-    <div class="control-group">
-      <label for="inputPassword3" class="col-sm-7 control-label">Password:</label>
-      <div class="col-sm-2">
-        <input type="password" id="inputPassword" name="login_password" placeholder="Password" required>
+  </div>
+  <div class="home-heigth">
+    <div class="form-group">
+      <label for="password" class="col-sm-2 control-label">Password:</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="password" placeholder="Enter your password">
       </div>
     </div>
-    <div class="control-group">
-      <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default">Sign in</button>
-      </div>
-    </div>
-  </form>
+  </div>
+  
 </div>
