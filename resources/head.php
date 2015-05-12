14ae5fae79d8b679683f34bd1928101329c47d3b
Maerill <efriem@WS-INF-STUD-04.bbcnet.ch>
2015-05-12 08:45:04 +0200

    added reset file

--- resources/head.php
+++ resources/head.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" href="resources/defaultstyle.css" type="text/css">
 <link rel="stylesheet" type="text/css" href="bootstrap-3.3.4-dist/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="resources/styleRaphi.css">
+<link rel="stylesheet" type="text/css" href="resources/reset.css">
