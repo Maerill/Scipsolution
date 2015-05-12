f928f9140fcd62ba8948e7e83759524331d31bd2
Maerill <efriem@WS-INF-STUD-04.bbcnet.ch>
2015-05-12 10:40:28 +0200

    Finished navigation

--- resources/pageHeader.php
+++ resources/pageHeader.php
@@ -1,8 +1,16 @@
-<nav>
-	<ul class="list-inline">
-		<li class="col-md-9"><img src="pictures/logo.png" height="50"></li>
-		<li class="col-md-1"><a href="?site=profil">Profil</a></li>
-		<li class="col-md-1"><a href="?site=overview">Overview</a></li>
-		<li class="col-md-1"><a href="?site=Logout">Logout</a></li>
-	</ul>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <a class="navbar-brand" href="?site=overview">
+        	<img alt="Brand" height="20" src="pictures/logo.png">
+      	</a>
+    </div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    	<ul class="nav navbar-nav navbar-right">
+      		<li><a href="?site=profil">Profil</a></li>
+      		<li><a href="?site=overview">Overview</a></li>    		
+      		<li><a href="?site=logout">Logout</a></li>
+      	</ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
 </nav>
--- sites/home.php
+++
@@ -1,2 +0,0 @@
-<h1>Willkommen bei Scip</h1>
-<button class="btn btn-primary" type="submit">Press Me!</button>
