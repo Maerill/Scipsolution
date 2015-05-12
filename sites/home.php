5fc48919bd9ea980c79e38e4d3128956c11321dd
Raphiii <raphael.spanyar@bluewin.ch>
2015-05-12 15:55:34 +0200

    homesite

--- sites/home.php
+++ sites/home.php
@@ -1,22 +1,25 @@
 <!-- Login Formular -->
-<img class="center_picture" src="pics/defaultProfilePic.png" width="200" height="150" />
+<div class="center_picture">
+  <img class="" src="pics/defaultProfilePic.png" width="200">
+</div>
 <div class="center_2">
-<form action="index.php" method="post" name="login_form" class="form-horizontal">
-  <div class="control-group">
-    <div class="controls">
-      <input type="username" id="inputUsername" name="login_username" placeholder="Benutzername" required>
-    </div>
-  </div>
-  <div class="control-group">
-    <div class="controls">
-      <input type="password" id="inputPassword" name="login_password" placeholder="Passwort" required>
-    </div>
-  </div>
+  <form action="index.php" method="post" name="login_form" class="form-horizontal">
     <div class="control-group">
-    <div class="controls">
-      <button type="submit" class="btn btn-inverse"><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Los gehts&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</strong></button>
+      <label for="inputEmail3" class="col-sm-5 control-label">Username:</label>
+      <div class="col-sm-2">
+        <input type="username" id="inputUsername" name="login_username" placeholder="Username" required>
+      </div>
     </div>
-  </div>
-  </div>
-</form>
+    <div class="control-group">
+      <label for="inputPassword3" class="col-sm-7 control-label">Password:</label>
+      <div class="col-sm-2">
+        <input type="password" id="inputPassword" name="login_password" placeholder="Password" required>
+      </div>
+    </div>
+    <div class="control-group">
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Sign in</button>
+      </div>
+    </div>
+  </form>
 </div>
