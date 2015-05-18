7d9490aa3ebc8e0a4681385fae4ba6cc0342d561
Raphiii <raphael.spanyar@bluewin.ch>
2015-05-18 09:33:20 +0200

    created register site

---
+++ sites/register.php
@@ -0,0 +1,55 @@
+<!-- Register Formular -->
+<div class="home-form">
+  <div class="home-heigth">
+    <div class="form-group">
+      <label for="username" class="col-sm-3 control-label">Username:</label>
+      <div class="col-sm-9">
+        <input type="text" class="form-control" id="username" placeholder="Enter your username">
+      </div>
+    </div>
+  </div>
+  <div class="home-heigth">
+    <div class="form-group">
+      <label for="Gender" class="col-sm-3 control-label">Gender:</label>
+      <div class="col-sm-9">
+      <label class="radio-inline">
+        <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="male" checked>Male</label>
+      <label class="radio-inline">
+          <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="female">Female</label>
+      </div>
+    </div>
+  </div>
+  <div class="home-heigth">
+    <div class="form-group">
+      <label for="E-Mail" class="col-sm-3 control-label">E-Mail:</label>
+      <div class="col-sm-9">
+        <input type="email" class="form-control" id="E-Mail" placeholder="Enter your E-Mail">
+      </div>
+    </div>
+  </div>
+  <div class="home-heigth">
+    <div class="form-group">
+      <label for="Birthday" class="col-sm-3 control-label">Birthday:</label>
+      <div class="col-sm-9">
+        <input type="date" class="form-control" id="Birthday" placeholder="Enter your birthday">
+      </div>
+    </div>
+  </div>
+  <div class="home-heigth">
+    <div class="form-group">
+      <label for="Phone number" class="col-sm-3 control-label">Phone number:</label>
+      <div class="col-sm-9">
+        <input type="tel" class="form-control" id="password" placeholder="Enter your phone number">
+      </div>
+    </div>
+  </div>
+  <div class="home-heigth">
+    <div class="form-group">
+      <label for="password" class="col-sm-3 control-label">Password:</label>
+      <div class="col-sm-9">
+        <input type="password" class="form-control" id="password" placeholder="Enter your password">
+      </div>
+    </div>
+  </div>
+  
+</div>
