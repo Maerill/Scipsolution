7d79ea346a794566808080bf7d9f02d3e0d857f7
Maerill <efriem@WS-INF-STUD-04.bbcnet.ch>
2015-05-18 10:27:32 +0200

    deleted link to file
    file was already deleted

--- resources/head.php
+++ resources/head.php
@@ -2,5 +2,4 @@
 <title>Scip</title>
 <link rel="stylesheet" href="resources/defaultstyle.css" type="text/css">
 <link rel="stylesheet" type="text/css" href="bootstrap-3.3.4-dist/css/bootstrap.css">
-<link rel="stylesheet" type="text/css" href="resources/styleRaphi.css">
 <link rel="stylesheet" type="text/css" href="resources/reset.css">
