30590d691da5739ff1058ffa2fe2737f62898a55
Maerill <efriem@WS-INF-STUD-04.bbcnet.ch>
2015-05-18 09:34:33 +0200

    Changed footer styling

--- index.php
+++ index.php
@@ -7,34 +7,40 @@ Startpage, every other page gets loaded in.
     <?php require_once("resources/head.php"); ?>
   </head>
   <body>
-  <?php 
-  		if (!empty($_GET['site']) && $_GET['site'] !== 'home' && $_GET['site'] !== 'register') {
-  			require_once("resources/pageNavbar.php"); 
-  		}else{
-        require_once("resources/loginNavbar.php");
-      }
-  ?>
-  <?php require_once("resources/pageHeader.php"); ?>
-  <div class="container">
-	<?php
-		// Get page folder and set default page
-		$pagefolder = 'sites/';
-		$defaultpage = 'home';
+    <div class="wrapper">
+      <?php 
+      		if (!empty($_GET['site']) && $_GET['site'] !== 'home' && $_GET['site'] !== 'register') {
+      			require_once("resources/pageNavbar.php"); 
+      		}else{
+            require_once("resources/loginNavbar.php");
+          }
+      ?>
 
-		if (!empty($_GET['site'])) {	// Check if there is a site
-			$page = $_GET["site"];
-		} else {
-			$page = $defaultpage;
-		}
+      <?php require_once("resources/pageHeader.php"); ?>
 
-		if (!file_exists($pagefolder . $page .".php")) {
-            $page = $defaultpage.".php";
-        }
+      <div class="container content">
 
-        // Get content
-        include $pagefolder . basename($page.".php");
-  ?>
-  </div>
-  	<?php require_once("resources/footer.php"); ?>
+    	<?php
+    		// Get page folder and set default page
+    		$pagefolder = 'sites/';
+    		$defaultpage = 'home';
+
+    		if (!empty($_GET['site'])) {	// Check if there is a site
+    			$page = $_GET["site"];
+    		} else {
+    			$page = $defaultpage;
+    		}
+
+    		if (!file_exists($pagefolder . $page .".php")) {
+                $page = $defaultpage.".php";
+            }
+
+            // Get content
+            include $pagefolder . basename($page.".php");
+      ?>
+
+      </div>
+      <?php require_once("resources/footer.php"); ?>
+    </div>
   </body>
 </html>
