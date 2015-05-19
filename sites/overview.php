816ea2ca34353c1b3c169e026d8e82f309355789
Raphiii <raphael.spanyar@bluewin.ch>
2015-05-19 13:02:10 +0200

    added pics overview

--- sites/overview.php
+++ sites/overview.php
@@ -1,19 +1,24 @@
 <div class="container-fluid">
-	<div class="overview-heading">
-		<h1>All pics!</h1>
-	</div>
-	<div class="overview-piccollegtion">
-		<div class="col-md-3 overview-inlineblock">
-			<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
+	<div class="row">
+		<div class="overview-heading">
+			<h1>All pics!</h1>
 		</div>
-		<div class="col-md-3 overview-inlineblock">
-			<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
-		</div>
-		<div class="col-md-3 overview-inlineblock">
-			<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
-		</div>
-		<div class="col-md-3 overview-inlineblock">
-			<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
+		<div class="overview-piccollegtion">
+			<div class="overview-inlineblock">
+				<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
+			</div>
+			<div class="overview-inlineblock">
+				<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
+			</div>
+			<div class="overview-inlineblock">
+				<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
+			</div>
+			<div class="overview-inlineblock">
+				<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
+			</div>
+			<div class="overview-inlineblock">
+				<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
+			</div>
 		</div>
 	</div>
 </div>
