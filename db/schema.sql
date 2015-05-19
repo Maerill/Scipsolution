5eb465487fe0f0ecdfc1dc6f57bc2c86e7d32c12
Raphiii <raphael.spanyar@bluewin.ch>
2015-05-19 16:54:21 +0200

    try sql

--- db/schema.sql
+++ db/schema.sql
@@ -30,12 +30,13 @@ USE scip;
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_pictures` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10)  NOT NULL AUTO_INCREMENT,
   `picture` blob NOT NULL,
-  `userId` int(10) unsigned NOT NULL,
-  `likes` int(10) unsigned DEFAULT NULL,
-  `dislikes` int(10) unsigned DEFAULT NULL,
-  `comments` varchar(1023) DEFAULT NULL
+  `userId` int(10)  NOT NULL,
+  `likes` int(10)  DEFAULT NULL,
+  `dislikes` int(10)  DEFAULT NULL,
+  `comments` varchar(1023) DEFAULT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -45,46 +46,26 @@ CREATE TABLE IF NOT EXISTS `tbl_pictures` (
 --
 
 CREATE TABLE IF NOT EXISTS `tbl_users` (
-  `id` int(10) unsigned NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(31) NOT NULL,
   `password` varchar(31) NOT NULL,
   `gender` tinyint(1) unsigned DEFAULT NULL,
   `mail` varchar(127) DEFAULT NULL,
   `birthday` date DEFAULT NULL,
   `phonenumber` int(11) DEFAULT NULL,
-  `pictureId` int(11) DEFAULT NULL
+  `pictureId` int(11) DEFAULT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indizes der exportierten Tabellen
---
-
---
--- Indizes für die Tabelle `tbl_pictures`
+-- Constraints der Tabelle `tbl_pictures`
 --
 ALTER TABLE `tbl_pictures`
-ADD PRIMARY KEY (`id`);
+ADD CONSTRAINT `tbl_pictures_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `tbl_users` (`id`);
 
---
--- Indizes für die Tabelle `tbl_users`
---
 ALTER TABLE `tbl_users`
-ADD PRIMARY KEY (`id`), ADD KEY `pictureId` (`pictureId`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `tbl_pictures`
---
-ALTER TABLE `tbl_pictures`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT für Tabelle `tbl_users`
---
-ALTER TABLE `tbl_users`
-MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+ADD CONSTRAINT `tbl_users_ibfk_1` FOREIGN KEY (`pictureId`) REFERENCES `tbl_pictures` (`id`);
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
