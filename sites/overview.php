25f6c7c50197f2efd72426269058c560c6e9aac9
Maerill <efriem@WS-INF-STUD-04.bbcnet.ch>
2015-05-19 13:19:33 +0200

    changed and finished overview
    without php or loading just static.

--- sites/overview.php
+++ sites/overview.php
@@ -1,8 +1,5 @@
 <div class="container-fluid">
 	<div class="row">
-		<div class="overview-heading">
-			<h1>All pics!</h1>
-		</div>
 		<div class="overview-piccollegtion">
 			<div class="overview-inlineblock">
 				<img class="" src="http://preprod.picture-organic-clothing.com/wp-content/uploads/2015/03/4-encore.png" width="200">
