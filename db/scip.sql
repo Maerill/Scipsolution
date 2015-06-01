25963e24d436e37c66b46fcf4dd24f3b19f64052
Raphiii <raphael.spanyar@bluewin.ch>
2015-06-01 13:45:04 +0200

    create database

--- db/scip.sql
+++ db/scip.sql
@@ -16,6 +16,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
+CREATE Database IF NOT EXISTS scip;
+USE scip;
+
 --
 -- Database: `scip`
 --
